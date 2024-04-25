d6910bf9c5d285a5af50a247d7d70018ae4e44e4
MatthewHunt84 <matthew.hunt@wundermanthompson.com>
2024-04-25 13:56:22 -0600

    initial commit

---
+++ Sources/PackageDocDemo/PackageDocDemo.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class DemoClass {
+    /// A function that says hello
+    /// This is a useless description of a function that says hello
+
+    public func sayHello() {
+        print("hello")
+    }
+}
